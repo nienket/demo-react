39dfb297da2fbbb5bae215e2f64e9e836d3100cd
nienket <79614475+nienket@users.noreply.github.com>
2023-02-07 04:42:25 +0700

    3 commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBIjWa9mHVvVcMwViYDJrwVUkHVLhff_9M",
-  authDomain: "fir-744da.firebaseapp.com",
-  projectId: "fir-744da",
-  storageBucket: "fir-744da.appspot.com",
-  messagingSenderId: "353739360072",
-  appId: "1:353739360072:web:0a88f3e2a25d475d40d57d",
-  measurementId: "G-KL389SZKT9",
+  apiKey: "AIzaSyCawhsDcP1SpW39wRqqGbhzRixHm2v3xy8",
+  authDomain: "astrology-c0fb2.firebaseapp.com",
+  projectId: "astrology-c0fb2",
+  storageBucket: "astrology-c0fb2.appspot.com",
+  messagingSenderId: "275199646634",
+  appId: "1:275199646634:web:3c99fda34dbd3a764fe9d3",
+  measurementId: "G-H1NDJJFFSX"
 };
 
 // Initialize Firebase
